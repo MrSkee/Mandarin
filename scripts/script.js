a41044ddc336431fae4140ac89bb0c9d12931e26
MrSkee <skee66499@gmail.com>
2017-03-03 18:49:15 -0500

    v0.1.1 Working fade-in/out nav-bar

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+/*var body = document.querySelector('body');
+var body_container = body.querySelector('body-container');
+var body_container_height = getComputedStyle(body_container).height.split('px')[0];
+var hidden_class = 'hidden';
+
+function nav_barAppear(e) {
+
+  if(window.pageYOffset >= body_container_height / 2) {
+    body_container.classList.remove(hidden);
+  }
+
+  if(window.pageYOffset < body_container_height /2) {
+    body_container.classList.add(hidden);
+  }
+}
+
+window.addEventListener('scroll', nav_barAppear, false);
+
+console.log("test");*/
+
+
+var $nav_bar = $('.nav-bar');
+var offset_value = $('.header-container').height();
+
+function navSlide() {
+  var scroll_top = $(window).scrollTop();
+
+  if (scroll_top >= offset_value) {
+    $nav_bar.removeClass('hidden');
+  }
+  else {
+    $nav_bar.addClass('hidden');
+  }
+}
+
+$(window).scroll(navSlide);
